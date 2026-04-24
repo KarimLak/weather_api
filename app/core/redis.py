cfd692523deb02e226416c0baed1f8cb5b20e5d8
KarimLak <karim.lakhal@polymtl.ca>
2026-04-24 13:18:47 -0400

    adding docker compose

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,8 +1,9 @@
+import os
 import redis
 
 redis_client = redis.Redis(
-    host="localhost",
-    port=6379,
+    host=os.getenv("REDIS_HOST"),
+    port=os.getenv("REDIS_PORT"),
     db=0,
     decode_responses=True,
 )
